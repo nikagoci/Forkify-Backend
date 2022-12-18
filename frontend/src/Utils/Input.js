7d3d4605ec2bebbcc4dcdf1ec7edec72953255a8
nikagoci <86802785+nikagoci@users.noreply.github.com>
2022-12-18 11:10:41 +0400

    TopForkify

--- frontend/src/Utils/Input.js
+++ frontend/src/Utils/Input.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from "@mui/material";
+import { Stack } from "@mui/system";
+import React from "react";
+
+import SearchIcon from "@mui/icons-material/Search";
 
 const Input = () => {
   return (
-    <div>Input</div>
-  )
-}
+    <Stack direction="row" position='relative' width='500px' height='55px' >
+      <input placeholder="Search over 1,000,000 recipes" style={{width: '100%', fontSize: '1rem', border: 'none', borderRadius:'10rem', paddingLeft: '30px'}} />
+      <Stack direction='row' position='absolute' top={0} right={0} height='100%' justifyContent='space-between' >
+        <Button variant="contained" color="primary" sx={{padding:'10px 45px', borderRadius:'10rem', backgroundImage: 'linear-gradient(to right bottom, #FBDB89, #F48982)'}}>
+          <SearchIcon sx={{position: 'absolute', top: '25%', left: '10%', color: '#fff'}} />
+          Search
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Input
+export default Input;
--- frontend/src/components/TopForkify.js
+++ frontend/src/components/TopForkify.js
@@ -1,24 +1,25 @@
-import Input from '../Utils/Input'
-import { Stack } from '@mui/system'
-import React from 'react'
+import Input from "../Utils/Input";
+import { Stack } from "@mui/system";
+import React from "react";
 
-import Logo from '../img/logo.png';
-import Icon from '../img/icons.svg';
+import Logo from "../img/logo.png";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const TopForkify = () => {
   return (
-    <Stack direction='row'>
-        <Stack width='200px'>
-            <img src={Logo} alt='logo' width='100%' height='100%' />
-        </Stack>
-        <Stack>
-            <Input />
-        </Stack>
-        <Stack>
-            <img src={Icon} alt='icon' width='100%' height='100%' />
-        </Stack>
+    <Stack direction="row" justifyContent='space-between' padding='20px 40px'>
+      <Stack width="150px">
+        <img src={Logo} alt="logo" width="100%" height="100%" />
+      </Stack>
+      <Stack>
+        <Input />
+      </Stack>
+      <Stack direction='row' position='relative' top={0} left={0}>
+        <FavoriteIcon sx={{color: 'red', fontSize: '40px'}} />
+        <Stack position='absolute' bottom='5px' right='-1px' width='20px' height='20px' backgroundColor='rgba(0,0,0,0.4)' color='#fff' borderRadius='20px' justifyContent='center' alignItems='center' >0</Stack>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default TopForkify
+export default TopForkify;
