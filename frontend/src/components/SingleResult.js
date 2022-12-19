9d2434d2250944acca1b578934129f2e82e30b1a
nikagoci <86802785+nikagoci@users.noreply.github.com>
2022-12-19 15:56:46 +0400

    add fake data to result

--- frontend/src/components/SingleResult.js
+++ frontend/src/components/SingleResult.js
@@ -1,19 +1,53 @@
-import { Typography } from '@mui/material'
-import { Stack } from '@mui/system'
-import React from 'react'
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import React from "react";
 
-const SingleResult = () => {
+const SingleResult = ({title, publisher, img}) => {
   return (
-    <Stack direction='row' justifyContent='center' paddingY='15px' sx={{cursor: 'pointer' ,':hover' : { backgroundColor: '#F9F5F3', transform: 'translateY(-5px)', transition: '0.3s all ease-in-out'}}}>
-        <Stack width='50px' height='50px' marginRight='20px'>
-            <img width='100%' height='100%' style={{borderRadius: '50%'}} src='https://i.picsum.photos/id/487/300/300.jpg?hmac=MgzJisyJqfVbHTxGIxVhLvpo9Rmakw2x2CR7tv4qkeM' alt='Unsplash' />
-        </Stack>
-        <Stack>
-            <Typography variant='subtitle1' component='h6' fontSize='1.1rem' color='#F59A83' textTransform='capitalize'>Best Pizza Dough</Typography>
-            <Typography variant='subtitle2' component='h6' fontSize='.8rem' color='#968B87' textTransform='capitalize'>101 CoockBooks</Typography>
-        </Stack>
+    <Stack
+      direction="row"
+      paddingY="15px"
+      paddingLeft='35px'
+      sx={{
+        cursor: "pointer",
+        ":hover": {
+          backgroundColor: "#F9F5F3",
+          transform: "translateY(-5px)",
+          transition: "0.3s all ease-in-out",
+        },
+      }}
+    >
+      <Stack width="50px" height="50px" marginRight="20px">
+        <img
+          width="100%"
+          height="100%"
+          style={{ borderRadius: "50%" }}
+          src={img}
+          alt={title}
+        />
+      </Stack>
+      <Stack>
+        <Typography
+          variant="subtitle1"
+          component="h6"
+          fontSize="1.1rem"
+          color="#F59A83"
+          textTransform="capitalize"
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          component="h6"
+          fontSize=".8rem"
+          color="#968B87"
+          textTransform="capitalize"
+        >
+          {publisher}
+        </Typography>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default SingleResult
+export default SingleResult;
