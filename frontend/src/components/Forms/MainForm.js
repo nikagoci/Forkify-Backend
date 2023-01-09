57f35d32ecf989fac3d282f4bed790004a5b955b
nikagoci <dato.svanidze2005@mail.ru>
2023-01-09 12:02:30 +0400

    added error/loading on recipe add

--- frontend/src/components/Forms/MainForm.js
+++ frontend/src/components/Forms/MainForm.js
@@ -1,16 +1,20 @@
 import { Stack } from "@mui/system";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import React from "react";
+import React, { useState } from "react";
 import FormData from "./FormData";
 import FormIngredients from "./FormIngredients";
 
-import axios from 'axios'
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const MainForm = () => {
+  const [isSent, setIsSent] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [recipeId, setRecipeId] = useState(null);
+
   const schema = yup.object().shape({
     title: yup.string().required("Title is required"),
     source_url: yup.string().required("Source is required"),
@@ -43,68 +47,138 @@ const MainForm = () => {
   const completedData = (data) => {
     let ingredients = [];
 
-    for(let i = 1; i <= 6; i++){
-      const ingredient = data[`ingredient${i}`]
+    for (let i = 1; i <= 6; i++) {
+      const ingredient = data[`ingredient${i}`];
 
-      delete data[`ingredient${i}`]
+      delete data[`ingredient${i}`];
 
-      if(ingredient){
+      if (ingredient) {
         ingredients.push(ingredient);
       }
-
     }
-    data.ingredients =ingredients;
+    data.ingredients = ingredients;
 
-    return data
-  }
+    return data;
+  };
 
-  const addRecipe = async(data) => {
-      try {
-        const response = await axios.post('http://localhost:5000/api/v1/recipes', data);
-        return response
-      } catch (error) {
-        console.log(error)
-      }
-  }
-  
+  const addRecipe = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/recipes",
+        data
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const onSubmit = (data) => {
     const fullData = completedData(data);
-    
+
     addRecipe(fullData)
+      .then((res) => {
+        if (res.status === 201) {
+          setIsSent(true);
+          setRecipeId(res.data.recipe._id)
+        }
+      })
+      .catch((err) => {
+        if (err) {
+          setIsError(true);
+        }
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack direction="row" columnGap={10}>
-        <FormData register={register} errors={errors} />
-        <FormIngredients register={register} errors={errors} />
-      </Stack>
-      <Stack direction="row" justifyContent="center" position="relative">
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{
-            padding: "10px 25px 10px 70px",
-            borderRadius: "10rem",
-            backgroundImage:
-              "linear-gradient(to right bottom, #FBDB89, #F48982)",
-          }}
-          type="submit"
+    <>
+      {!isError && !isSent && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack direction="row" columnGap={10}>
+            <FormData register={register} errors={errors} />
+            <FormIngredients register={register} errors={errors} />
+          </Stack>
+          <Stack direction="row" justifyContent="center" position="relative">
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                padding: "10px 25px 10px 70px",
+                borderRadius: "10rem",
+                backgroundImage:
+                  "linear-gradient(to right bottom, #FBDB89, #F48982)",
+              }}
+              type="submit"
+            >
+              <CloudUploadIcon
+                sx={{
+                  position: "absolute",
+                  top: "25%",
+                  left: "20%",
+                  color: "#fff",
+                }}
+                type="submit"
+              />
+              Upload
+            </Button>
+          </Stack>
+        </form>
+      )}
+      {isSent && (
+        <Stack justifyContent="center" alignItems="center">
+          <Typography
+            variant="h3"
+            component="h2"
+            marginTop="120px"
+            color="#346019"
+          >
+            Data Successfuly sent
+          </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                padding: "10px 50px",
+                borderRadius: "10rem",
+                backgroundImage:
+                  "linear-gradient(to right bottom, #FBDB89, #F48982)",
+                marginTop: '120px'
+              }}
+              type="submit"
+              onClick={() => console.log(recipeId)}
+            >
+              See Added Recipe
+            </Button>
+        </Stack>
+      )}
+      {isError && (
+        <Stack justifyContent="center" alignItems="center">
+        <Typography
+          variant="h3"
+          component="h2"
+          marginTop="120px"
+          color="#346019"
         >
-          <CloudUploadIcon
+          Data Sent Failed
+        </Typography>
+          <Button
+            variant="contained"
+            color="primary"
             sx={{
-              position: "absolute",
-              top: "25%",
-              left: "20%",
-              color: "#fff",
+              padding: "10px 50px",
+              borderRadius: "10rem",
+              backgroundImage:
+                "linear-gradient(to right bottom, #FBDB89, #F48982)",
+              marginTop: '120px'
             }}
             type="submit"
-          />
-          Upload
-        </Button>
+            onClick={() => console.log(recipeId)}
+          >
+            Go Back 
+          </Button>
       </Stack>
-    </form>
+      )}
+    </>
   );
 };
 
